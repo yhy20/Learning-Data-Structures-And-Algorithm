dc6c716cc5dda8d3e16da41093b7e2fe6636a9ee
yhy <yanhaoyu251@gmail.com>
2022-04-13 21:47:43 +0800

    "add 64 bit mul"

--- AcWing/Advanced_Algorithm/0x00.Basic_Algorithm/0x01.Bit_Manipulation/02.AcWing90/method2.cpp
+++ AcWing/Advanced_Algorithm/0x00.Basic_Algorithm/0x01.Bit_Manipulation/02.AcWing90/method2.cpp
@@ -18,23 +18,14 @@ const ll mod = 1e9 + 7;
     cin.tie(0);                  \
     cout.tie(0);
 
-map<PII, bool> existed;
-int n, p, h, m;
-int d[5005];
 int main() {
     ios;
-    cin >> n >> p >> h >> m;
-    rep(i, 1, m + 1) {
-        int a, b;
-        cin >> a >> b;
-        if(a > b) swap(a, b);
-        if(existed[{a, b}]) continue;
-        d[a + 1]--, d[b]++;
-        existed[{a, b}] = true;
-    }
-    rep(i, 1, n + 1) {
-        d[i] += d[i - 1];
-        cout << d[i] + h << '\n';
-    }
+    ll a, b, p;
+    cin >> a >> b >> p;
+    a %= p;
+    b %= p;
+    ll c = (long double)a * b / p;
+    ll ans = a * b - c * p;
+    cout << ans;
     return 0;
 }
--- AcWing/Advanced_Algorithm/0x00.Basic_Algorithm/0x01.Bit_Manipulation/03.AcWing91/method1.cpp
+++ AcWing/Advanced_Algorithm/0x00.Basic_Algorithm/0x01.Bit_Manipulation/03.AcWing91/method1.cpp
@@ -18,23 +18,8 @@ const ll mod = 1e9 + 7;
     cin.tie(0);                  \
     cout.tie(0);
 
-map<PII, bool> existed;
-int n, p, h, m;
-int d[5005];
 int main() {
     ios;
-    cin >> n >> p >> h >> m;
-    rep(i, 1, m + 1) {
-        int a, b;
-        cin >> a >> b;
-        if(a > b) swap(a, b);
-        if(existed[{a, b}]) continue;
-        d[a + 1]--, d[b]++;
-        existed[{a, b}] = true;
-    }
-    rep(i, 1, n + 1) {
-        d[i] += d[i - 1];
-        cout << d[i] + h << '\n';
-    }
+    
     return 0;
 }
